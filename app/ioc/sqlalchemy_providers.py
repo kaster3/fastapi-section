f34d81e8cb83344a450148709ce1eda3e53d6ddc
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-03 00:30:47 +0300

    create providers

--- app/ioc/sqlalchemy_providers.py
+++ app/ioc/sqlalchemy_providers.py
@@ -1,10 +1,30 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, AsyncIterator
 
+import aiohttp
 from dishka import Provider, Scope, from_context, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.database.db_helper import DataBaseHelper
+from app.core.db.db_helper import DataBaseHelper
+from app.core.db_repository import AlchemyRepository, IDBRepository
+from app.core.excel_parser import ExcelParser
+from app.core.parser import Parser
+from app.core.service import Service
 from app.core.settings import Settings
+
+
+class ServiceProvider(Provider):
+    scope = Scope.REQUEST
+    settings = from_context(Settings, scope=Scope.APP)
+
+    service = provide(Service)
+    db = provide(AlchemyRepository, provides=IDBRepository)
+    parser = provide(Parser)
+    excel_parser = provide(ExcelParser)
+
+    @provide
+    async def get_http_session(self) -> AsyncIterator[aiohttp.ClientSession]:
+        async with aiohttp.ClientSession() as session:
+            yield session
 
 
 class SQLAlchemyProvider(Provider):
@@ -18,10 +38,6 @@ class SQLAlchemyProvider(Provider):
     ) -> DataBaseHelper:
         return DataBaseHelper(
             url=str(settings.db.url),
-            echo=settings.db.echo,
-            echo_pool=settings.db.echo_pool,
-            pool_size=settings.db.pool_size,
-            max_overflow=settings.db.max_overflow,
         )
 
     @provide(scope=Scope.REQUEST)
