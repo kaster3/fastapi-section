d8a86a0b5bd0e232acda3c59f09f06607bea597f
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-03 01:57:08 +0300

    add args

--- app/ioc/sqlalchemy_providers.py
+++ app/ioc/sqlalchemy_providers.py
@@ -38,6 +38,10 @@ class SQLAlchemyProvider(Provider):
     ) -> DataBaseHelper:
         return DataBaseHelper(
             url=str(settings.db.url),
+            echo=settings.db.echo,
+            echo_pool=settings.db.echo_pool,
+            pool_size=settings.db.pool_size,
+            max_overflow=settings.db.max_overflow,
         )
 
     @provide(scope=Scope.REQUEST)
