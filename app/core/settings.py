6ac63a000696c60eaccd770439089104f256c6c4
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-08-03 01:56:55 +0300

    add the base path constant

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 from typing import Literal
 
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_PATH = Path(__file__).parent.parent.parent
 
 
 class ApiV1Prefix(BaseModel):
@@ -48,7 +51,10 @@ class Links(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".template.env", ".env"),
+        env_file=(
+            BASE_PATH / ".template.env",
+            BASE_PATH / ".env",
+        ),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="FASTAPI__",
